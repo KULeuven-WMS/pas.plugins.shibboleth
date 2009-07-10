9e106ec26cc625495cc38bc6f6ea58252301ab0c
Jean-François Roche <jfroche@affinitic.be>
2009-07-10 15:21:41 +0000

    add buildout & set depedencies

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='pas.plugins.shibboleth',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Products.PluggableAuthService'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
