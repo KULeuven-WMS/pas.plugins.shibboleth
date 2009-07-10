f4cb6daa1a911fce846ee73424a954785ecbc9c0
Jean-François Roche <jfroche@affinitic.be>
2009-07-10 08:45:38 +0000

    initial import

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='pas.plugins.shibboleth',
+      version=version,
+      description="Shibboleth groups and properties handler for Pluggable Authentication Service and PlonePAS",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='pas shibboleth plone',
+      author='KULeuven ICTS',
+      author_email='wms@icts.kuleuven.be',
+      url='https://wms.cc.kuleuven.be/repo2/wms/packages/pas.plugins.shibboleth',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pas', 'pas.plugins'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
