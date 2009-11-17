8ad4a2d08ee9949688b085d0509d2e38752a96d2
peterjacobs <peter@peterjacobs.be>
2009-11-17 16:00:52 +0000

    better initialize the plugin to be able to use it in z2

--- src/pas/plugins/shibboleth/__init__.py
+++ src/pas/plugins/shibboleth/__init__.py
@@ -1,8 +1,11 @@
 from Products.PluggableAuthService import registerMultiPlugin
 from AccessControl.Permissions import add_user_folders
-from pas.plugins.shibboleth import group, property
+from pas.plugins.shibboleth import group, property, enumeration
+
+
 registerMultiPlugin(group.ShibGroupManager.meta_type)
 registerMultiPlugin(property.ShibUserPropertiesManager.meta_type)
+registerMultiPlugin(enumeration.ShibUserEnumerationManager.meta_type)
 
 
 def initialize(context):
@@ -18,5 +21,12 @@ def initialize(context):
                           constructors = (
                               property.manage_addShibUserPropertiesManagerForm,
                               property.manage_addShibUserProperties),
+                          visibility = None,
+                          icon='')
+    context.registerClass(enumeration.ShibUserEnumerationManager,
+                          permission = add_user_folders,
+                          constructors = (
+                              enumeration.manage_addShibUserEnumerationManagerForm,
+                              enumeration.manage_addShibUserEnumerationManager),
                           visibility = None,
                           icon='')
